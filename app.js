f5a9c0db3ad84873ef87889cd041876d41591d82
codefug <86544979+codefug@users.noreply.github.com>
2024-02-14 15:54:01 +0900

    add jest

--- app.js
+++ app.js
@@ -1,53 +1,58 @@
-const dotenv = require('dotenv');
-const passport = require('passport');
+const dotenv = require("dotenv");
+const passport = require("passport");
 dotenv.config();
 
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const indexRouter = require('./routes/index');
-const authRouter = require('./routes/auth');
-const userRouter = require('./routes/user');
-const session = require('express-session');
-const passportConfig = require('./passport');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const indexRouter = require("./routes/index");
+const authRouter = require("./routes/auth");
+const userRouter = require("./routes/user");
+const session = require("express-session");
+const passportConfig = require("./passport");
 const app = express();
 
-
 // 포트 이름 세팅
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
+app.use(cors({ origin: true, credentials: true }));
 passportConfig();
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-  resave:false,
-  saveUninitialized:false,
-  secret:process.env.COOKIE_SECRET,
-  cookie:{
-    httpOnly: true,
-    secure: false,
-  },
-}));
-app.use(passport.authenticate('session'));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.authenticate("session"));
 app.use(indexRouter);
 app.use(authRouter);
 app.use(userRouter);
 // app.use('/signup',signupRouter);
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   // render the error page
   res.status(err.status || 500);
   res.send("오류 입니다.");
 });
 
-app.listen(app.get("port"),(req,res,next)=>{
-  console.log(app.get("port")+"에서 대기중");
-})
+app.listen(app.get("port"), (req, res, next) => {
+  console.log(app.get("port") + "에서 대기중");
+});
 
 module.exports = app;
--- passport/localStrategy.js
+++ passport/localStrategy.js
@@ -4,13 +4,13 @@ const localStrategy = require('passport-local');
 const { readDB } = require('../controller/db');
 
 module.exports = () => {
-    passport.use(new localStrategy(function verify(username, password, cb) {
+    passport.use(new localStrategy(async function verify(username, password, cb) {
         try {
             const user = readDB('userData', 'users', { userid: username }, false);
             if (!user.userid) {
                 return cb(null, false);
             }
-            bcrypt.compare(password, user.password, function (err, result) {
+            await bcrypt.compare(password, user.password, function (err, result) {
                 if (result!==undefined){
                     return cb(null,user)
                 }
